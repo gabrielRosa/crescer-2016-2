68835bd7335c9b5f70315d89b418680bddc52bd3
Gabriel <rosa.gabriel@cwi.com.br>
2016-09-28 15:01:41 -0300

    fix(CestoDeLembas): corrigir calculos

--- src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -1,13 +1,11 @@
-public class CestoDeLembas
-{
+public class CestoDeLembas{
     private int quantidade;
+
     public CestoDeLembas(int quantidade){
         this.quantidade = quantidade;
     }
 
     public boolean podeDividirEmPares(){
-        int personagens=2;
-        double div =this.quantidade/personagens;
-        return (div>1 && div%2==0);
+        return (this.quantidade>2 && this.quantidade<=100 && this.quantidade%2==0);
     }
 }
