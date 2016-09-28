4db847e93c5d076a9b50c9d033f1b672a5dc90d3
Gabriel <rosa.gabriel@cwi.com.br>
2016-09-28 15:50:11 -0300

    fix(Dwarf): retirar métodos não usados

---
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -0,0 +1,14 @@
+public class Dwarf{
+    private int vida;
+    public Dwarf(){
+        this.vida=110;
+    }
+    
+    public void perderVida(){
+        this.vida-=10;
+    }
+
+    public int getVida(){
+        return this.vida;
+    }
+}
