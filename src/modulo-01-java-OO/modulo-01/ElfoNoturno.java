29e0140439475c473be037beb8fd53c3d32b381e
Gabriel <rosa.gabriel@cwi.com.br>
2016-10-04 15:06:24 -0300

    refactor(ElfoNoturno):  retirar instruções repetidas

--- src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
@@ -2,15 +2,19 @@ public class ElfoNoturno extends Elfo{
     public ElfoNoturno (String nome){
         super(nome);
     }
-    
+
     public ElfoNoturno (String nome, int quantidade){
         super(nome, quantidade);
     }
 
     @Override
     public void atirarFlecha(Dwarf dwarf){
-        super.atirarFlecha(dwarf);
-        super.xp+=2;
-        super.vida= this.vida - (super.vida/100)*5;
+        if(super.getStatus() == Status.VIVO){
+            super.atirarFlecha(dwarf,3);
+            super.vida= this.vida - (super.vida/100)*5;
+            if((int)super.vida == 0){
+                super.status = Status.MORTO;
+            }
+        }
     }
 }
