65577adfa6ab95ac78f0f67b71ee47efd5d6a666
Unknown <gabrielfr97@gmail.com>
2016-10-09 23:12:37 -0300

    feat(EstrategiaAtaqueIntercalado): implementar a classe

--- src/modulo-01-java-OO/modulo-01/EstrategiaAtaqueIntercaladoTest.java
+++ src/modulo-01-java-OO/modulo-01/EstrategiaAtaqueIntercaladoTest.java
@@ -5,14 +5,66 @@ import org.junit.Test;
 import java.util.*;
 
 public class EstrategiaAtaqueIntercaladoTest{
+
     @Test
-    public void getOrdemDeAtaqueVerificaOrdemNoturnoEDepoisVerde() throws ContingenteDesproporcionalException{
+    public void getOrdemDeAtaqueVerificaOrdemNoturnoEDepoisVerdeCom4() throws ContingenteDesproporcionalException{
         //arrange
         EstrategiaAtaqueIntercalado e = new EstrategiaAtaqueIntercalado();
         List<Elfo> elfo = new ArrayList<>(Arrays.asList(new ElfoNoturno("b"),new ElfoVerde("a"), new ElfoVerde("a"), new ElfoNoturno("b")));
         //act
         List<Elfo> ret = e.getOrdemDeAtaque(elfo);
         //assert
-        assertEquals(elfo.get(1), ret.get(0));
+        assertEquals(elfo.get(1), ret.get(1));
     }
+
+    @Test
+    public void getOrdemDeAtaqueVerificaOrdemNoturnoEDepoisVerdeCom3() throws ContingenteDesproporcionalException{
+        //arrange
+        EstrategiaAtaqueIntercalado e = new EstrategiaAtaqueIntercalado();
+        List<Elfo> elfo = new ArrayList<>(Arrays.asList(new ElfoNoturno("b"),new ElfoVerde("a"), new ElfoVerde("a")));
+        //act
+        try{
+            List<Elfo> ret = e.getOrdemDeAtaque(elfo);
+        } catch(ContingenteDesproporcionalException ex){
+            //assert
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void getOrdemDeAtaqueVerificaOrdemNoturnoEDepoisVerdeCom8() throws ContingenteDesproporcionalException{
+        //arrange
+        EstrategiaAtaqueIntercalado e = new EstrategiaAtaqueIntercalado();
+        List<Elfo> elfo = new ArrayList<>(Arrays.asList(new ElfoNoturno("b"),new ElfoVerde("a"), new ElfoVerde("a"), new ElfoNoturno("b"),new ElfoNoturno("b"),new ElfoVerde("a"), new ElfoVerde("a"), new ElfoNoturno("b")));
+        //act
+        List<Elfo> ret = e.getOrdemDeAtaque(elfo);
+        //assert
+        assertEquals(elfo.get(0), ret.get(0));
+    }
+
+    @Test
+    public void getOrdemDeAtaqueNula() throws ContingenteDesproporcionalException{
+        //arrange
+        EstrategiaAtaqueIntercalado e = new EstrategiaAtaqueIntercalado();
+        List<Elfo> elfo = new ArrayList<>();
+        //act
+        List<Elfo> ret = e.getOrdemDeAtaque(elfo);
+        //assert
+        assertEquals(new ArrayList<>(), ret);
+    }
+
+    @Test
+    public void getOrdemDeAtaqueVerificaOrdemSoNoturno() throws ContingenteDesproporcionalException{
+        //arrange
+        EstrategiaAtaqueIntercalado e = new EstrategiaAtaqueIntercalado();
+        List<Elfo> elfo = new ArrayList<>(Arrays.asList(new ElfoNoturno("b")));
+        //act
+        try{
+            List<Elfo> ret = e.getOrdemDeAtaque(elfo);
+        } catch(ContingenteDesproporcionalException ex){
+            //assert
+            assertTrue(true);
+        }
+    }
+
 }
