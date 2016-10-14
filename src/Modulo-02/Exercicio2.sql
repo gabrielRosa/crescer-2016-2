bad48306573b65441a3e4581cdd7856b478ba605
Gabriel <rosa.gabriel@cwi.com.br>
2016-10-14 14:51:48 -0300

    fix(Exercicio2): consertar erros

--- src/Modulo-02/Exercicio2.sql
+++ src/Modulo-02/Exercicio2.sql
@@ -1,6 +1,5 @@
-
---1)ainda nao terminado
-select NomeEmpregado, DATEDIFF(Month,DataAdmissao,convert(datetime,'2000/12/31')) MesesTrabalhados
+--1)
+select NomeEmpregado, DataAdmissao, DATEDIFF(Month,DataAdmissao,convert(datetime,'2000/12/31')) as MesesTrabalhados
 from empregado
 where  DataAdmissao between '1980/05/01' and '1982/01/20' 
 --2)
@@ -11,19 +10,28 @@ having count(cargo) = (select max (C.Cargo)
 from (select count (cargo) as Cargo
 from empregado
 group by (cargo)) as c)
+
+/*
+Select top 1 with ties
+	cargo,
+	count(1) as Total
+from Empregado
+group by cargo
+order by Total desc;
+*/
 --3) 
-select uf, count(nome)
+select uf, count(1) as TotalCidades
 from cidade 
 group by uf
 
 --4)
-select uf, count(nome)
+select nome, uf, count(1) as Duplicadas
 from cidade 
-group by uf
-having count(nome)>2
+group by Nome, uf
+having count(nome)>1
 
 --5)
-select max(IDAssociado)+1
+select isnull(max(IDAssociado),0)+1 as Proximo_ID
 from associado
 --6)
 select nomeempregado, salario,
@@ -35,6 +43,17 @@ then 15
 end,
 case when salario > 2326
 then 27.5
-end
+end as Imposto
 from empregado
+/*
+select NomeEmpregado,
+	 Salario,
+	 case when salario <= 1164 then 0
+		when salario <= 2336 then 15
+		else 27.5
+		end as Imposto
+		From empregado;
+
+*/
+
 
