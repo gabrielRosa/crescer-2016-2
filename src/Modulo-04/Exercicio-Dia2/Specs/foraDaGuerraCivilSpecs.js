42427695dff131d9900653156c129e8b9d10aab7
Gabriel <rosa.gabriel@cwi.com.br>
2016-10-26 13:08:50 -0200

    teste(Herois): adicionar testes

--- src/Modulo-04/Exercicio-Dia2/Specs/foraDaGuerraCivilSpecs.js
+++ src/Modulo-04/Exercicio-Dia2/Specs/foraDaGuerraCivilSpecs.js
@@ -5,14 +5,18 @@ describe('foraDaGuerraCivil()', function() {
     let resultado = herois5.foraDaGuerraCivil();
     expect(resultado[0]).toEqual('Guardians of the Galaxy');
   });
-  /*
-  //como testar um objeto
+
   it('retorna o objeto personagem que mais possui histórias em quadrinhos publicadas', function() {
-    let herois5 = new Herois(marvel);
+    let dadosHerois = [
+      { name: 'Debug Destruidor', comics: { available: 12 } },
+      { name: 'Tipagem Maravilhosa', comics: { available: 39 } },
+      { name: 'Compilador Prateado', comics: { available: 3 } }
+    ]
+    let herois5 = new Herois(dadosHerois);
     let resultado = herois5.maisPublicado();
-    expect(resultado[0]).toEqual();
+    expect(resultado).toEqual({ name: 'Tipagem Maravilhosa', comics: { available: 39 } });
   });
-  */
+
 
   it('retorna a média de páginas de todos quadrinhos dos personagens', function() {
     let herois5 = new Herois(marvel);
@@ -20,6 +24,15 @@ describe('foraDaGuerraCivil()', function() {
     expect(resultado).toEqual(37.03125);
   });
 
-  //duvidas de como testar objetos
+  it(' retorna o objeto comic mais caro entre todos personagens', function() {
+    let dadosHerois = [
+      { name: 'Debug Destruidor', comics: { itens: { prices: {price : 100 }} } },
+      { name: 'Tipagem Maravilhosa', comics:  { itens: { prices: {price : 1000 }} } },
+      { name: 'Compilador Prateado', comics:  { itens: { prices: {price : 10}} } }
+    ]
+    let herois5 = new Herois(dadosHerois);
+    let resultado = herois5.maisPublicado();
+    expect(resultado).toEqual(  { name: 'Tipagem Maravilhosa', comics:  { itens:{ prices:{price : 100,2000}} } });
+  });
 
 });
