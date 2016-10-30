ea1c27bae67af0f0a27a95ee3c5e513bf3413fb2
Unknown <gabrielfr97@gmail.com>
2016-10-29 23:59:58 -0200

    feat(MarioKart):implementar nível 4

---
+++ src/Modulo-05/MarioKart/MarioKart/SKYFUSION.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corredores;
+using MarioKart.Equipamentos;
+
+namespace Karts
+{
+    public class SKYFUSION : Kart, IEquipamento
+    {
+        public SKYFUSION(Corredor corredor) : base(corredor)
+        {
+        }
+
+        public int BonusEquipameto
+        {
+            get
+            {
+                int bonusEquipameto = 0;
+                foreach (IEquipamento obj in Equipamentos)
+                {
+                    bonusEquipameto += obj.BonusEquipameto;
+                }
+                int bonusDeHabilidadeDoCorredor = this.CalculaBonusCorredor();
+                int velocidadeTotal = bonusEquipameto + bonusDeHabilidadeDoCorredor;
+                if (velocidadeTotal > 12)
+                {
+                    velocidadeTotal += 5;
+                }
+                return velocidadeTotal;
+            }
+        }
+    }
+}
