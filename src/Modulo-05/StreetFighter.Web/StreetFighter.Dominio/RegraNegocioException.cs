c47f1d2071475c86a74fa6b59cec09e652d5b5fd
Gabriel <rosa.gabriel@cwi.com.br>
2016-11-03 17:37:39 -0200

    Não Acabado

---
+++ src/Modulo-05/StreetFighter.Web/StreetFighter.Dominio/RegraNegocioException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StreetFighter.Dominio
+{
+    public class RegraNegocioException : Exception
+    {
+        public RegraNegocioException(string msg) : base(msg)
+        { }
+    }
+}
